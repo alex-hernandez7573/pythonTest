d2e148344e06aec5016de5d45b4cdca2e5a329cc
Alex Hernandez <alex.hernandez7573@gmail.com>
2021-09-16 19:17:45 -0500

    simple syntax practice with python, just started, will continue the rest of the evening

---
+++ Syntax.py
@@ -0,0 +1,6 @@
+if 5 > 2:
+    print("Five is greater than two")
+
+if 5 < 2:
+    print("this will not print, because this is untrue")
+# first comment: this above if statement will not print because it is untrue
---
+++ Variables.py
@@ -0,0 +1,46 @@
+a = 3
+b = "Alex"
+c = True
+d = False
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+# will print the type
+
+e = str(555)
+f = int(111)
+g = float(222)
+
+print(e)
+print(f)
+print(g)
+
+h, i, j = "Maroon", "Violet", "Orange"
+
+print(h)
+print(i)
+print(j)
+
+# collection
+
+movies = ["Atomic Blonde", "Kate", "Heat"]
+k, l, m = movies
+print(k)
+print(l)
+print(m)
+
+n = "Alex Hernandez"
+
+
+def thisFunction():
+    print("hello my name is " + n)
+
+
+thisFunction()
